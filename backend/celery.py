43f892623faee82aa84e23f03920214b7623d37f
Ethan Harstad <ethanharstad@gmail.com>
2014-04-24 07:06:02 +0400

    Bring up to PEP8 standards

--- backend/celery.py
+++ backend/celery.py
@@ -2,22 +2,22 @@ from __future__ import absolute_import
 from celery import Celery
 
 app = Celery(
-  'backend',
-  broker='amqp://',
-  backend='amqp',
-  include=[
-    'backend.datastoreTasks'
-  ]
+    'backend',
+    broker='amqp://',
+    backend='amqp',
+    include=[
+        'backend.datastoreTasks'
+    ]
 )
 
 app.conf.update(
-  CELERY_ENABLE_UTC=True,
-  CELERY_RESULT_SERIALIZER='json',
-  CELERY_TASK_RESULT_EXPIRES=3600,
-  CELERY_ACCEPT_CONTENT = ['json'],
-  CELERY_TRACK_STARTED=True,
-  CELERY_TASK_SERIALIZER='json',
+    CELERY_ENABLE_UTC=True,
+    CELERY_RESULT_SERIALIZER='json',
+    CELERY_TASK_RESULT_EXPIRES=3600,
+    CELERY_ACCEPT_CONTENT=['json'],
+    CELERY_TRACK_STARTED=True,
+    CELERY_TASK_SERIALIZER='json',
 )
 
 if __name__ == '__main__':
-  app.start()
+    app.start()
--- backend/datastoreTasks.py
+++ backend/datastoreTasks.py
@@ -1,17 +1,31 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import gridfs
 from backend.celery import app
 from backend.mongoTask import MongoTask
 
+
 @app.task(base=MongoTask)
 def downloadSounding(modelRun, forecastHours):
-  modelTime = datetime.utcfromtimestamp(modelRun)
-  filename = '{0:04d}-{1:02d}-{2:02d}-{3:02d}z-{4:03d}h'.format(
-    modelTime.year, modelTime.month, modelTime.day,
-    modelTime.hour, forecastHours
-  )
-  db = downloadSounding.mongo.soundings
-  fs = gridfs.GridFS(db)
-  handle = fs.put(filename, filename=filename)
-  print fs.get(handle)
-  return str(handle)
+    modelTime = datetime.utcfromtimestamp(modelRun)
+    fileprefix = 'http://nomads.ncep.noaa.gov/pub/data/nccf/com/gfs/prod/'
+    filedir = 'gfs.{year:04d}{month:02d}{day:02d}{run:02d}/'.format(
+        year=modelTime.year,
+        month=modelTime.month,
+        day=modelTime.day,
+        run=modelTime.hour
+    )
+    filename = 'gfs.{run:02d}.pgrbf{hour:02d}.grib2'.format(
+        run=modelTime.hour,
+        hour=forecastHours
+    )
+    soundingTime = modelTime + timedelta(hours=forecastHours)
+    savename = '{year:04d}-{month:02d}-{day:02d}-{hour:02d}'.format(
+        year=soundingTime.year,
+        month=soundingTime.month,
+        day=soundingTime.day,
+        hour=soundingTime.hour
+    )
+    db = downloadSounding.mongo.soundings
+    fs = gridfs.GridFS(database=db, collection='files')
+    handle = fs.put(fileprefix + filedir + filename, filename=savename)
+    return str(handle)
