526a299dfcf97b6f1ed310afe4a142e2c7a9bf66
Ethan Harstad <ethanharstad@gmail.com>
2014-04-24 06:10:32 +0400

    GridFS testing

--- backend/datastoreTasks.py
+++ backend/datastoreTasks.py
@@ -1,15 +1,17 @@
 from datetime import datetime
+import gridfs
 from backend.celery import app
+from backend.mongoTask import MongoTask
 
-@app.task
+@app.task(base=MongoTask)
 def downloadSounding(modelRun, forecastHours):
   modelTime = datetime.utcfromtimestamp(modelRun)
   filename = '{0:04d}-{1:02d}-{2:02d}-{3:02d}z-{4:03d}h'.format(
     modelTime.year, modelTime.month, modelTime.day,
     modelTime.hour, forecastHours
   )
-  return filename
-
-@app.task
-def processSounding(filename):
-  print 'Processed: {}'.format(filename)
+  db = downloadSounding.mongo.soundings
+  fs = gridfs.GridFS(db)
+  handle = fs.put(filename, filename=filename)
+  print fs.get(handle)
+  return str(handle)
