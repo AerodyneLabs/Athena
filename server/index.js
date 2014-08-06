9294adf9ba4979fd81b32cc0efa6cc704323c189
Ethan Harstad <ethanharstad@gmail.com>
2014-08-06 01:03:58 +0000

    Expose db on request object

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
 var restify = require('restify');
 var socketio = require('socket.io');
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/atmosphere');
+
 
 function version(req, res, next) {
 	res.send({
@@ -11,11 +15,15 @@ function version(req, res, next) {
 var server = restify.createServer();
 var io = socketio.listen(server);
 
+server.use(function(req, res, next) {
+	req.db = db;
+	next();
+});
+
 server.get('api/version', version);
 
 // Get sounding from database
 server.get('api/sounding/:timestamp/:latitude/:longitude', function(req, res, next) {
-
 });
 
 server.listen(8080, function() {
