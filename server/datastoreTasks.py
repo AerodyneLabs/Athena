eef5cafe4b906e11349cb283d0ecba48914a7829
Ethan Harstad <ethanharstad@gmail.com>
2014-08-10 04:12:27 +0000

    Comment download_sounding

--- server/datastoreTasks.py
+++ server/datastoreTasks.py
@@ -6,9 +6,6 @@ from mongoTask import MongoTask
 import gridfs
 
 
-TEMP_PREFIX = 'data/temp/'
-
-
 def get_url(model_run, forecast_hours):
     server_address = 'http://nomads.ncep.noaa.gov/pub/data/nccf/com/gfs/prod/'
     model_folder = 'gfs.{year:04d}{month:02d}{day:02d}{run:02d}/'.format(
@@ -26,18 +23,23 @@ def get_url(model_run, forecast_hours):
 
 @app.task(base=MongoTask)
 def download_forecast(model_run, forecast_hours):
+    # Get URL of file on web
     file_url = get_url(model_run, forecast_hours)
+    # Compute forecast datetime
     forecast_time = model_run + timedelta(hours=forecast_hours)
+    # Determine human readable file name
     file_name = model_run.strftime('%Y%m%d%H') + '-' + forecast_time.strftime('%Y%m%d%H') + '.grib2'
-    db = download_forecast.mongo.atmosphere
-    fs = gridfs.GridFS(db)
+    # Get GridFS instance
+    fs = gridfs.GridFS(download_forecast.mongo.atmosphere)
+    # Download the file and save to GridFS
     request = get(file_url, stream=True)
     if request.status_code == 200:
         with fs.new_file(filename=file_name, analysis=model_run,
                 forecast=forecast_time) as file:
             for chunk in request.iter_content(1024):
                 file.write(chunk)
-    return TEMP_PREFIX + file_name
+    # Return the file name
+    return file_name
 
 
 @app.task(base=MongoTask)
