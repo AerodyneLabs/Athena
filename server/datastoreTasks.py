62768e87275f98ca9493a84ed38047d4790e527d
Ethan Harstad <ethanharstad@gmail.com>
2014-08-11 22:13:32 +0000

    Add process_file to extract from grib

--- server/datastoreTasks.py
+++ server/datastoreTasks.py
@@ -22,6 +22,44 @@ def get_url(model_run, forecast_hours):
     return server_address + model_folder + forecast_file
 
 
+def process_file(input_name):
+    # Open grib file
+    grib_file = grib(input_name)
+    # Select records from grib file
+    u_sel = grib_file.select(shortName='u', typeOfLevel='isobaricInhPa')
+    v_sel = grib_file.select(shortName='v', typeOfLevel='isobaricInhPa')
+    t_sel = grib_file.select(shortName='t', typeOfLevel='isobaricInhPa')
+    h_sel = grib_file.select(shortName='gh', typeOfLevel='isobaricInhPa')
+    # Extract values from records
+    vals = lambda x: x.values
+    u_val = map(vals, u_sel)
+    v_val = map(vals, v_sel)
+    t_val = map(vals, t_sel)
+    h_val = map(vals, h_sel)
+    p_val = map(lambda x: x.level * 100, u_sel)
+    lats = u_sel[0].distinctLatitudes
+    lons = u_sel[0].distinctLongitudes
+    # Close input file
+    grib_file.close()
+
+    # Determine output file name
+    output_name = input_name.split('.')[0] + '.npz'
+    # Save values to output file
+    np.savez_compressed(
+        output_name,
+        h=h_val,
+        p=p_val,
+        t=t_val,
+        u=u_val,
+        v=v_val,
+        lat=lats,
+        lon=lons
+    )
+
+    # Return output file name
+    return output_name
+
+
 @app.task(base=MongoTask, bind=True)
 def download_forecast(self, model_run, forecast_hours):
     # Convert time string to datetime object
