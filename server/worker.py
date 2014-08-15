0909ec42e91a76d6ebd5042bfd5ad7439ed1a22e
Ethan Harstad <ethanharstad@gmail.com>
2014-08-15 23:09:29 +0000

    Register serializer

--- server/worker.py
+++ server/worker.py
@@ -1,5 +1,16 @@
 from __future__ import absolute_import
 from celery import Celery
+from kombu.serialization import register
+import serializer
+
+
+register(
+    'myjson',
+    serializer.celery_dumps,
+    serializer.celery_loads,
+    content_type='application/x-myjson',
+    content_encoding='utf-8'
+)
 
 app = Celery(
     'backend',
@@ -12,11 +23,11 @@ app = Celery(
 
 app.conf.update(
     CELERY_ENABLE_UTC=True,
-    CELERY_RESULT_SERIALIZER='json',
-    CELERY_TASK_RESULT_EXPIRES=3600,
-    CELERY_ACCEPT_CONTENT=['json'],
+    CELERY_RESULT_SERIALIZER='myjson',
+    CELERY_ACCEPT_CONTENT=['myjson'],
+    CELERY_TASK_SERIALIZER='myjson',
     CELERY_TRACK_STARTED=True,
-    CELERY_TASK_SERIALIZER='json',
+    CELERY_TASK_RESULT_EXPIRES=3600
 )
 
 if __name__ == '__main__':
