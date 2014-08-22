c5f682dbfabbadbabe61bd29265d190beab1ffaa
Ethan Harstad <ethanharstad@gmail.com>
2014-08-22 02:47:31 +0000

    Add download_latest function

--- server/airspaceTasks.py
+++ server/airspaceTasks.py
@@ -1,5 +1,9 @@
 from datetime import date
 from math import floor
+from requests import get
+
+
+TEMP_DIR = 'data/'
 
 
 def get_latest_date():
@@ -9,3 +13,23 @@ def get_latest_date():
     seq = int(floor((cur_ord - ref_ord) / 56))
     latest = date.fromordinal(seq * 56 + ref_ord)
     return latest
+
+
+def download_latest():
+    # Get file url
+    latest_date = get_latest_date()
+    prefix = 'https://nfdc.faa.gov/webContent/56DaySub/'
+    filename = 'aixm5.1.zip'
+    folder = '{0}/'.format(latest_date)
+    url = prefix + folder + filename
+    # Download file
+    request = get(url, stream=True)
+    total_length = request.headers.get('content-length')
+    cur_length = 0
+    if request.status_code == 200:
+        with open(TEMP_DIR + filename, 'wb') as file:
+            for chunk in request.iter_content(2**18):
+                file.write(chunk)
+                cur_length += len(chunk)
+    # Return filename
+    return TEMP_DIR + filename
