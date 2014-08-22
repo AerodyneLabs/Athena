192b01018bb8bea13f33df2c0f3579825c4327ce
Ethan Harstad <ethanharstad@gmail.com>
2014-08-22 02:29:14 +0000

    Change to get latest date

--- server/airspaceTasks.py
+++ server/airspaceTasks.py
@@ -2,14 +2,10 @@ from datetime import date
 from math import floor
 
 
-def get_url():
+def get_latest_date():
     # Find latest date on the 56 day cycle
     ref_ord = date(2014, 5, 29).toordinal()
     cur_ord = date.today().toordinal()
     seq = int(floor((cur_ord - ref_ord) / 56))
     latest = date.fromordinal(seq * 56 + ref_ord)
-    # Generate url
-    prefix = 'https://nfdc.faa.gov/webContent/56DaySub/'
-    filename = 'aixm5.1.zip'
-    folder = '{0}/'.format(latest)
-    return prefix + folder + filename
+    return latest
