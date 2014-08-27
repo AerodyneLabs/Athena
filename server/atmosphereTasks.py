68120592f7d419853066564a34b5ba5244df2ead
Ethan Harstad <ethanharstad@gmail.com>
2014-08-27 22:47:17 +0000

    Add task to download full model run

--- server/atmosphereTasks.py
+++ server/atmosphereTasks.py
@@ -2,6 +2,7 @@ from requests import get
 from datetime import datetime, timedelta
 from worker import app
 from pygrib import open as grib
+from celery import group
 from mongoTask import MongoTask
 import gridfs
 import numpy as np
@@ -191,3 +192,27 @@ def download_forecast(self, analysis_time, forecast_hours):
 
     # Return the file name
     return npz_file_name
+
+
+@app.task()
+def download_model_run(analysis_time):
+    forecast_hours = [
+        0, 3, 6, 9, 12,
+        15, 18, 21, 24,
+        27, 30, 33, 36,
+        39, 42, 45, 48,
+        51, 54, 57, 60,
+        63, 66, 69, 72,
+        75, 78, 81, 84,
+        87, 90, 93, 96,
+        99, 102, 105, 108,
+        111, 114, 117, 120,
+        123, 126, 129, 132,
+        135, 138, 141, 144,
+        147, 150, 153, 156,
+        159, 162, 165, 168
+    ]
+    job = group(
+        download_forecast.s(analysis_time, hour)
+        for hour in forecast_hours)
+    res = job.apply_async()
