f11334014361d257e4f0b9c03439436df675a0de
Ethan Harstad <ethanharstad@gmail.com>
2015-01-18 16:13:53 -0600

    Rounding in unit helper
    Closes #26

--- app/helpers/units-value.js
+++ app/helpers/units-value.js
@@ -1,8 +1,33 @@
 import Ember from 'ember';
 import convert from 'athena/helpers/units';
+import round from 'athena/helpers/round-number';
 
-export function unitsValue(system, type, value) {
-  return convert(system, type, value).value;
+export function unitsValue(system, type, value, digits) {
+  var out = convert(system, type, value).value;
+  if(digits) {
+    if(type === 'altitude') {
+      if(system === 'metric') {
+        return round(out, -2);
+      } else {
+        return round(out, -1);
+      }
+    } else if(type === 'temperature') {
+      return round(out, -2);
+    } else if(type === 'distance') {
+      return round(out, -3);
+    } else if(type === 'pressure') {
+      if(system === 'metric') {
+        return round(out, -1);
+      } else {
+        return round(out, -4);
+      }
+    } else if(type === 'lowSpeed') {
+      return round(out, -2);
+    } else if(type === 'highSpeed') {
+      return round(out, -1);
+    }
+  }
+  return out;
 }
 
 export default Ember.Handlebars.makeBoundHelper(unitsValue);
---
+++ tests/unit/helpers/round-number-test.js
@@ -0,0 +1,11 @@
+import {
+  roundNumber
+} from 'athena/helpers/round-number';
+
+module('RoundNumberHelper');
+
+// Replace this with your real tests.
+test('it works', function() {
+  var result = roundNumber(42);
+  ok(result);
+});
