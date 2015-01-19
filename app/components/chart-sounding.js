c7b1e86304bb572505e5ba35991938326f7050fe
Ethan Harstad <ethanharstad@gmail.com>
2015-01-19 00:20:45 -0600

    Add pressure scale to sounding chart
    Closes #30

--- app/components/chart-sounding.js
+++ app/components/chart-sounding.js
@@ -5,7 +5,7 @@ export default Ember.Component.extend({
 	tagName: 'svg',
 	attributeBindings: 'width height'.w(),
 
-	margin: {top: 20, right: 20, bottom: 30, left: 70},
+	margin: {top: 20, right: 70, bottom: 30, left: 70},
 
 	w: function() {
 		return this.get('width') - this.get('margin.left') - this.get('margin.right');
@@ -30,44 +30,68 @@ export default Ember.Component.extend({
 		var units = this.get('units');
 		var altUnit = convert(units, 'altitude', 0).unit;
 		var tempUnit = convert(units, 'temperature', 0).unit;
+		var presUnit = convert(units, 'pressure', 0).unit;
 		var svg = d3.select('#'+this.get('elementId'));
 
-		var xScale = d3.scale.linear().range([0, width]);
-		xScale.domain(d3.extent(data, function(d) {
-			return convert(units, 'temperature', d['t']).value;
-		}));
-		var yScale = d3.scale.linear().range([height, 0]);
-		yScale.domain(d3.extent(data, function(d) {
-			return convert(units, 'altitude', d['h']).value;
-		}));
+		var tempScale = d3.scale.linear()
+			.range([0, width])
+			.domain(d3.extent(data, function(d) {
+				return convert(units, 'temperature', d['t']).value;
+			}));
+		var altScale = d3.scale.linear()
+			.range([height, 0])
+			.domain(d3.extent(data, function(d) {
+				return convert(units, 'altitude', d['h']).value;
+			}));
+		var presScale = d3.scale.log()
+			.range([0, height])
+			.domain(d3.extent(data, function(d) {
+				return convert(units, 'pressure', d['p']).value;
+			}));
 
-		var xAxis = d3.svg.axis().scale(xScale).orient('bottom');
-		var yAxis = d3.svg.axis().scale(yScale).orient('left');
+		var tempAxis = d3.svg.axis()
+			.scale(tempScale)
+			.orient('bottom');
+		var altAxis = d3.svg.axis()
+			.scale(altScale)
+			.orient('right');
+		var presAxis = d3.svg.axis()
+			.scale(presScale)
+			.orient('left')
+			.tickFormat(d3.format('d'));
 
 		var line = d3.svg.line()
 			.x(function(d) {
-				return xScale(convert(units, 'temperature', d['t']).value);
+				return tempScale(convert(units, 'temperature', d['t']).value);
 			})
 			.y(function(d) {
-				return yScale(convert(units, 'altitude', d['h']).value);
+				return altScale(convert(units, 'altitude', d['h']).value);
 			})
 			.interpolate('monotone');
 
 		svg.select('.data').attr('d', line(data));
-		svg.select('.axis.x')
-			.call(xAxis)
+		svg.select('.axis.temperature')
+			.call(tempAxis)
 			.select('text')
-			.attr('x', width)
+			.attr('x', '0.5em')
 			.attr('dy', '-.5em')
-			.attr('text-anchor', 'end')
+			.attr('text-anchor', 'start')
 			.text('Temperature (' + tempUnit + ')');
-		svg.select('.axis.y')
-			.call(yAxis)
+		svg.select('.axis.altitude')
+			.attr('transform', 'translate(' + width + ',0)')
+			.call(altAxis)
+			.select('text')
+			.attr('transform', 'rotate(-90)')
+			.attr('dy', '-.5em')
+			.style('text-anchor', 'end')
+			.text('Altitude (' + altUnit + ')');
+		svg.select('.axis.pressure')
+			.call(presAxis)
 			.select('text')
 			.attr('transform', 'rotate(-90)')
 			.attr('dy', '1em')
 			.style('text-anchor', 'end')
-			.text('Altitude (' + altUnit + ')');
+			.text('Pressure (' + presUnit + ')');
 	}.observes('units'),
 
 	didInsertElement: function() {
