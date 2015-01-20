17473712def21de2f027c7f724b7552845421b0a
Ethan Harstad <ethanharstad@gmail.com>
2015-01-19 21:54:52 -0600

    Add wind speed to sounding chart
    For #29

--- app/components/chart-sounding.js
+++ app/components/chart-sounding.js
@@ -6,6 +6,7 @@ export default Ember.Component.extend({
 	attributeBindings: 'width height'.w(),
 
 	margin: {top: 20, right: 70, bottom: 30, left: 70},
+	padding: 20,
 
 	w: function() {
 		return this.get('width') - this.get('margin.left') - this.get('margin.right');
@@ -15,6 +16,14 @@ export default Ember.Component.extend({
 		return this.get('height') - this.get('margin.top') - this.get('margin.bottom');
 	}.property('height'),
 
+	tempWidth: function() {
+		return Math.round(0.7 * (this.get('w') - this.get('padding')));
+	}.property('width'),
+
+	windWidth: function() {
+		return this.get('w') - this.get('tempWidth') - this.get('padding');
+	}.property('tempWidth'),
+
 	transformG: function() {
 		return "translate(" + this.get('margin.left') + "," + this.get('margin.top') + ")";
 	}.property(),
@@ -23,15 +32,25 @@ export default Ember.Component.extend({
 		return "translate(0,"+ this.get('h') +")";
 	}.property('h'),
 
+	transformWind: function() {
+		return "translate(" + (this.get('tempWidth') + this.get('padding')) + "," + this.get('h') + ")";
+	}.property('h'),
+
+	transformAlt: function() {
+		return "translate(" + this.get('w') + ",0)";
+	}.property('width'),
+
 	draw: function() {
-		var width = this.get('w');
 		var height = this.get('h');
 		var data = this.get('data');
 		var units = this.get('units');
+		var tempWidth = this.get('tempWidth');
+		var windWidth = this.get('windWidth');
+		var padding = this.get('padding');
 		var svg = d3.select('#'+this.get('elementId'));
 
 		var tempScale = d3.scale.linear()
-			.range([0, width])
+			.range([0, tempWidth])
 			.domain(d3.extent(data, function(d) {
 				return convert(units, 'temperature', d['t']).value;
 			}));
@@ -45,6 +64,11 @@ export default Ember.Component.extend({
 			.domain(d3.extent(data, function(d) {
 				return convert(units, 'pressure', d['p']).value;
 			}));
+		var windScale = d3.scale.linear()
+			.range([0, windWidth])
+			.domain(d3.extent(data, function(d) {
+				return convert(units, 'highSpeed', d['ws']).value;
+			}));
 
 		var tempAxis = d3.svg.axis()
 			.scale(tempScale)
@@ -56,6 +80,9 @@ export default Ember.Component.extend({
 			.scale(presScale)
 			.orient('left')
 			.tickFormat(d3.format('d'));
+		var windAxis = d3.svg.axis()
+			.scale(windScale)
+			.orient('bottom');
 
 		var line = d3.svg.line()
 			.x(function(d) {
@@ -64,16 +91,27 @@ export default Ember.Component.extend({
 			.y(function(d) {
 				return altScale(convert(units, 'altitude', d['h']).value);
 			})
-			.interpolate('monotone');
+			.interpolate('linear');
 
-		svg.select('.data').attr('d', line(data));
+		var wind = d3.svg.line()
+			.x(function(d) {
+				return windScale(convert(units, 'highSpeed', d['ws']).value) + tempWidth + padding;
+			})
+			.y(function(d) {
+				return altScale(convert(units, 'altitude', d['h']).value);
+			})
+			.interpolate('linear');
+
+		svg.select('.data.temperature').attr('d', line(data));
+		svg.select('.data.wind').attr('d', wind(data));
 		svg.select('.axis.temperature')
 			.call(tempAxis);
 		svg.select('.axis.altitude')
-			.attr('transform', 'translate(' + width + ',0)')
 			.call(altAxis);
 		svg.select('.axis.pressure')
 			.call(presAxis);
+		svg.select('.axis.wind')
+			.call(windAxis);
 	}.observes('units'),
 
 	didInsertElement: function() {
