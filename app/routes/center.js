863a540ab7febe5382555a23e2b066a0e521e510
Ethan Harstad <ethanharstad@gmail.com>
2015-02-04 15:04:02 -0600

    Fix random JSHint warnings

--- app/routes/center.js
+++ app/routes/center.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    return this.store.find('center', params.id)
+    return this.store.find('center', params.id);
   }
 });
