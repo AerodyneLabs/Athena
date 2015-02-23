f1d54ec6b0547ae32d65f3c8b8650408a049f798
Ethan Harstad <ethanharstad@gmail.com>
2015-02-23 06:53:03 +0000

    Add reverse geocode and altitude API endpoints

--- backend/index.js
+++ backend/index.js
@@ -312,7 +312,7 @@ server.get('api/navaids/:id', function(req, res, next) {
 	});
 });
 
-server.get('api/geocode', function(req, res, next) {
+server.get('api/geo/address', function(req, res, next) {
   var baseUrl = 'http://open.mapquestapi.com/geocoding/v1/address';
   var url = baseUrl + '?key=' + process.env.MAPQUEST_API_KEY;
   request.post({
@@ -333,6 +333,71 @@ server.get('api/geocode', function(req, res, next) {
       'type': 'Point',
       'coordinates': [loc.latLng.lng, loc.latLng.lat]
     }});
+    return next();
+  });
+});
+
+server.get('api/geo/reverse', function(req, res, next) {
+  var baseUrl = 'http://open.mapquestapi.com/geocoding/v1/reverse';
+  var url = baseUrl + '?key=' + process.env.MAPQUEST_API_KEY;
+  var coords = JSON.parse(req.query.location);
+  request.post({
+    uri: url,
+    json: true,
+    body: {
+      location: {
+        latLng: {
+          lat: coords[1],
+          lng: coords[0]
+        }
+      },
+      options: {
+        maxResults: 1,
+        thumbMaps: false
+      }
+    }
+  }, function(error, response, body) {
+    if(error) return next(error);
+
+    var address = body.results[0].locations[0];
+    res.send({'address': {
+      city: address.adminArea5,
+      county: address.adminArea4,
+      state: address.adminArea3,
+      country: address.adminArea1,
+      postalCode: address.postalCode
+    }});
+    return next();
+  });
+});
+
+server.get('api/geo/altitude', function(req, res, next) {
+  var baseUrl = 'http://open.mapquestapi.com/elevation/v1/profile';
+  var url = baseUrl + '?key=' + process.env.MAPQUEST_API_KEY;
+  var coords = JSON.parse(req.query.location);
+  var latlngs = [];
+  coords.forEach(function(x) {
+    latlngs.push(x[1]);
+    latlngs.push(x[0]);
+  });
+  request.post({
+    uri: url,
+    json: true,
+    body: {
+      latLngCollection: latlngs
+    }
+  }, function(error, response, body) {
+    if(error) return next(error);
+
+    var alts = body.elevationProfile;
+    var result = new Array(coords.length);
+    for(var i = 0; i < alts.length; i++) {
+      result[i] = {
+        type: 'Point',
+        coordinates: [coords[i][0], coords[i][1], alts[i].height]
+      };
+    }
+    res.send({'locations': result});
     return next();
   });
 });
