610ae7c8ac7e3924878b075bef2deb62f14cc9b1
Ethan Harstad <ethanharstad@gmail.com>
2015-03-02 13:58:09 -0600

    Fix prediction wizard 1 error/validation flow

--- app/prediction/wizard/1/controller.js
+++ app/prediction/wizard/1/controller.js
@@ -12,10 +12,30 @@ export default Ember.Controller.extend({
   needs: ['prediction/wizard'],
   actions: {
     next: function() {
+      var goodLat = false;
       var lat = this.parseLocation(this.get('latitudeInput'));
+      if(isNaN(lat)) {
+        this.set('latitudeError', true);
+      } else if(lat > 90 || lat < -90) {
+        this.set('latitudeError', true);
+      } else {
+        this.set('latitudeError', false);
+        goodLat = true;
+      }
+      var goodLon = false;
       var lon = this.parseLocation(this.get('longitudeInput'));
-      if(this.setLocation([lon, lat])) {
-        this.transitionToRoute('prediction.wizard.2');
+      if(isNaN(lon)) {
+        this.set('longitudeError', true);
+      } else if(lon < -180 || lon > 180) {
+        this.set('longitudeError', true);
+      } else {
+        this.set('longitudeError', false);
+        goodLon = true;
+      }
+      if(goodLat && goodLon) {
+        if(this.setLocation([lon, lat])) {
+          this.transitionToRoute('prediction.wizard.2');
+        }
       }
     },
     search: function() {
@@ -25,8 +45,14 @@ export default Ember.Controller.extend({
       Ember.$.get('api/geo/address', {
         address: this.get('searchInput')
       }).done(function(data) {
+        var coords = data.location.coordinates;
+        if(coords) {
+          controller.set('latitudeInput', coords[1]);
+          controller.set('longitudeInput', coords[0]);
+        } else {
+          controller.set('searchError', true);
+        }
         controller.set('searchError', false);
-        controller.setLocation(data.location.coordinates);
       }).fail(function() {
         controller.set('searchError', true);
       }).always(function() {
@@ -86,16 +112,6 @@ export default Ember.Controller.extend({
   setLocation: function(loc) {
     var latitude = loc[1];
     var longitude = loc[0];
-    if(latitude <= 90.0 && latitude >= -90.0) {
-      this.set('latitudeInput', latitude);
-    } else {
-      return false;
-    }
-    if(longitude <= 180.0 && longitude > -180.0) {
-      this.set('longitudeInput', longitude);
-    } else {
-      return false;
-    }
     this.set('controllers.prediction/wizard.location', Ember.create({
       longitude: longitude,
       latitude: latitude
