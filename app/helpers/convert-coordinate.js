250c6d7b20793a2be35a1e1c7a5d24bd50ebf11a
Ethan Harstad <ethanharstad@gmail.com>
2015-03-02 12:50:28 -0600

    Fix jshint error in convert-coordinate

--- app/helpers/convert-coordinate.js
+++ app/helpers/convert-coordinate.js
@@ -1,11 +1,12 @@
 import Ember from 'ember';
 
 export function convertCoordinate(value, style, type) {
+  var d, m, s, ret;
   if(style === 'DMS') {
-    var d = ~~value;
-    var m = ~~(Math.abs(value) * 60) % 60;
-    var s = (Math.abs(value) * 3600) % 60;
-    var ret = Math.abs(d) + '-' + m + '-' + s.toFixed(4);
+    d = ~~value;
+    m = ~~(Math.abs(value) * 60) % 60;
+    s = (Math.abs(value) * 3600) % 60;
+    ret = Math.abs(d) + '-' + m + '-' + s.toFixed(4);
     if(type === 'latitude') {
       if(d >= 0) {
         ret += 'N';
@@ -21,9 +22,9 @@ export function convertCoordinate(value, style, type) {
     }
     return ret;
   } else if(style === 'DM') {
-    var d = ~~value;
-    var m = Math.abs(value - d) * 60;
-    var ret = Math.abs(d) + '-' + m.toFixed(6);
+    d = ~~value;
+    m = Math.abs(value - d) * 60;
+    ret = Math.abs(d) + '-' + m.toFixed(6);
     if(type === 'latitude') {
       if(d >= 0) {
         ret += 'N';
