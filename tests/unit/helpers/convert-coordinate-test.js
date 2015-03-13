5b663c3107b3dae8bb883796dc9dc6323ea555ef
Ethan Harstad <ethanharstad@gmail.com>
2015-03-13 14:50:34 -0500

    conver-coordinate tests

--- tests/unit/helpers/convert-coordinate-test.js
+++ tests/unit/helpers/convert-coordinate-test.js
@@ -2,11 +2,26 @@ import {
   convertCoordinate
 } from '../../../helpers/convert-coordinate';
 import { module, test } from 'qunit';
+import closeEnough from '../../helpers/close-enough';
 
 module('ConvertCoordinateHelper');
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  var result = convertCoordinate(42);
-  assert.ok(result);
+test('inverts from signed decimal degrees', function() {
+  closeEnough(convertCoordinate('40.4461', null, null, true), 40.4461);
+  closeEnough(convertCoordinate('-79.9822', null, null, true), -79.9822);
+});
+
+test('inverts from decimal degrees', function() {
+  closeEnough(convertCoordinate('40.4461N', null, null, true), 40.4461);
+  closeEnough(convertCoordinate('79.9822W', null, null, true), -79.9822);
+});
+
+test('inverts from decimal minutes', function(assert) {
+  closeEnough(convertCoordinate('40-26.767N', null, null, true), 40.4461);
+  closeEnough(convertCoordinate('79-58.933W', null, null, true), -79.9822);
+});
+
+test('inverts from degrees-minutes-seconds', function() {
+  closeEnough(convertCoordinate('40-26.767N', null, null, true), 40.4461);
+  closeEnough(convertCoordinate('79-58.933W', null, null, true), -79.9822);
 });
