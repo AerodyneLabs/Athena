e52cd3d08234b7e34c05a004f465875efca3f0b1
Ethan Harstad <ethanharstad@gmail.com>
2015-07-05 23:28:52 -0500

    Enable default pagination on api

--- server/athena/settings.py
+++ server/athena/settings.py
@@ -113,3 +113,8 @@ CELERY_RESULT_BACKEND = 'rpc://'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 25,
+}
