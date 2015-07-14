77c325f712bbc6f15692de895521055c915c7826
Ethan Harstad <ethanharstad@gmail.com>
2015-07-13 20:23:46 -0500

    Switch celery from rabbitmq to redis

--- server/athena/settings.py
+++ server/athena/settings.py
@@ -108,8 +108,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Celery configuration
-BROKER_URL = 'amqp://guest:guest@localhost:5672//'
-CELERY_RESULT_BACKEND = 'rpc://'
+BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
